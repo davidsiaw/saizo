98bb56f8a5d5440c22b76d1375811b517f56409d
David Siaw <davidsiaw@gmail.com>
2022-08-29 11:25:02 +0000

    update

--- install.jl
+++ install.jl
@@ -1,3 +1,10 @@
 import Pkg;
+Pkg.add("PackageCompiler");
 Pkg.add("JuMP")
 Pkg.add("Cbc");
+
+using PackageCompiler;
+using JuMP;
+using Cbc;
+m = Model(Cbc.Optimizer);
+create_sysimage(sysimage_path="SysImage.so")
